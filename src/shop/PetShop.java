c0f1dc53598f25f4aab2c8dec99eee8deccde1b1
35385 <35385@DESKTOP-GA0VFQ8>
2019-04-11 17:36:54 +0100

    Here I've created a small stock pet shop application which allows users to buy products and it notifys when stock is gone for certain products.

---
+++ src/shop/PetShop.java
@@ -0,0 +1,58 @@
+package shop;
+import java.util.Scanner;
+
+/**
+ * 
+ *This file has been developed to display and give examples of the coding conventions set
+ *out in the handbook.
+ *
+ *This simple program is an inventory control systems that loops through a pet shops stock of tennis 
+ *balls and dog food, notifying the user when the product runs out of stock. 
+ * 
+ *
+*/
+
+public class PetShop {
+	 static Scanner scan = new Scanner(System.in); //This creates a scanner object 
+	 public static void main(String args[]) {
+		 
+	   
+		int tennisBalls = 10;
+		int dogFood = 2;
+	 
+	    for(int i = 0 ; i <= tennisBalls && i <= dogFood; i++ ) { //This for loop loops through stock until stock is gone
+		 
+		System.out.println("What do product would you like to buy ? Please Enter 1 for tennis balls OR 2 for dog Food");
+		String productChoice = scan.nextLine();
+		 
+		if (productChoice.equalsIgnoreCase("1")) {
+			
+			tennisBalls = tennisBalls - 1;
+			
+		}else if(productChoice.equalsIgnoreCase("2")) {
+			 
+			 dogFood = dogFood - 1;
+			 
+		}else System.out.println("INVALID ENTERY!!! Enter 1 or 2");
+		 
+		if (tennisBalls < 10 || dogFood < 10) {
+			
+			  System.out.println("There are " + tennisBalls + " tennis balls left");
+			  System.out.println("There are " + dogFood + " bags of dog food left");
+			  
+		}
+	 }
+	    
+	    if (tennisBalls == 0) {
+	    	
+		   System.out.println("Tennis balls are out of stock");
+		   
+	    }else if(dogFood == 0) {
+	    	
+		   System.out.println("Dog Food is out of stock");
+	   }
+	  }
+
+
+
+}
